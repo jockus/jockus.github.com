bc2e73140e55f8a93165f42ceedcaff02a9ba633
jockus <joakim.hentula@gmail.com>
2015-07-14 13:42:19 +0100

    Spacemacs dotfiles and private layer

---
+++ dotfiles/.emacs.d/private/jockus/config.el
@@ -0,0 +1,11 @@
+
+(defun insert-diary-entry (day-offset)
+  "Insert new org-mode diary entry at top off file, using day-offset as an offset to the current date."
+  (interactive)
+  (evil-goto-first-line)
+  (evil-open-below 1)
+  (evil-open-below 1)
+  (insert (format-time-string "* %d/%m/%y:" (time-add (current-time) (days-to-time day-offset))))
+  (evil-open-below 1)
+  (insert "** ")
+  (evil-insert 1))
---
+++ dotfiles/.emacs.d/private/jockus/keybindings.el
@@ -0,0 +1,4 @@
+
+(evil-leader/set-key "od" (lambda () (interactive) (insert-diary-entry 0)))
+(evil-leader/set-key "ot" (lambda () (interactive) (insert-diary-entry 1)))
+(evil-leader/set-key "gt" 'eyebrowse-next-window-config)
